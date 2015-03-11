8b6f946b643121f2439a0d5187d2ca44793b3230
Edgar_Terré <edgar_josue.t.-1@hotmail.com>
2015-03-11 17:00:49 -0600

    Agregada clase calculadora simple

---
+++ Practica1_2015/src/Calculadora.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void operacion(){
+		
+		Scanner n=new Scanner(System.in);
+		
+		String z="";
+		
+		//Ingreso de valores
+		System.out.println("Ingrese el primer numero");
+		int x=n.nextInt();
+		
+		System.out.println("Ingrese el segundo numero");
+		int y=n.nextInt();
+	
+		System.out.println("¿Que operacion desea realizar?"
+				+ "\n[s] Suma"
+				+ "\n[r] Resta"
+				+ "\n[m] Multiplicacion"
+				+ "\n[d] Division");
+		z=n.next();
+		//Opciones
+		switch (z) {
+		case "s":
+			System.out.println("La suma es: "+(x+y));
+			break;
+			
+		case "r":
+			System.out.println("La resta es: "+(x-y));
+			break;
+			
+		case "m":
+			System.out.println("La multiplicacion es: "+(x*y));
+			break;
+			
+		case "d":
+			System.out.println("La division es: "+(x/y));
+			break;
+
+		default:
+			System.err.println("Opcion invalida");
+			break;
+		}
+	}
+}
